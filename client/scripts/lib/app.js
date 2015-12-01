e4020fe056bf23b3497bc1293a93c0635b7f7c26
Sebastian <sebastian.treu@gmail.com>
2015-12-01 16:52:12 -0300

    Create app.js

---
+++ client/scripts/lib/app.js
@@ -0,0 +1,16 @@
+angular
+  .module('gevo-angular', [
+    'angular-meteor',
+    'ionic'
+  ]);
+ 
+if (Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+}
+else {
+  angular.element(document).ready(onReady);
+}
+ 
+function onReady() {
+  angular.bootstrap(document, ['gevo-angular']);
+}
